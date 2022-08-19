64ed97f638bbdd85427abf11610688efb4495fbc
HOAN_C <hoan.c9907@gmail.com>
2022-08-19 17:47:14 +0900

    fix Nav & footer padding

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,15 +4,15 @@ function Footer() {
   return (
     <footer className={classes.footer}>
       <div>
-        <p>
+        <p className={classes.article}>
           CEO : JEA HO CHOI / Contact : +8210-9459-6374 / Business Number :
           302-20-66624
-        </p>
-        <p>
+          <br />
           Address : JL. jababeka III G Bloc C17 BS, Kawasan Industri Jababeja
           Desa Pasir Gombong-Cikarang Utara Bekasi
+          <br />
+          Copyright : HOAN_C
         </p>
-        <p>Copyright : HOAN_C</p>
       </div>
     </footer>
   );
